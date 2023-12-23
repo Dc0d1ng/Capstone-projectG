4f1a8a48eedb385148129a82b6d2242cd12b0b5c
Isaac Ho <Isaachwho@gmail.com>
2023-12-23 14:19:38 -0800

    add react-router-dom and routes

--- r4g/src/App.jsx
+++ r4g/src/App.jsx
@@ -1,8 +1,18 @@
-import { useState } from "react";
 import "./App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" />
+          <Route path="/add-fav" />
+          <Route path="/remove-fav" />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ r4g/src/Pages/Landing.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const landing = () => {
+  return (
+    <>
+      <h1>landing</h1>
+    </>
+  );
+};
+
+export default landing;
