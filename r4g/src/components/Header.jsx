ef93f3f57d4301c8231972eab6b2a6fe6d87b57a
Isaac Ho <Isaachwho@gmail.com>
2023-12-23 15:38:44 -0800

    create header component

--- r4g/src/components/Header.jsx
+++ r4g/src/components/Header.jsx
@@ -1,14 +1,26 @@
 import React from "react";
+import "./Header.scss";
+import Logo from "../assets/r4g.png";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <>
-      <h1 className="header">R0LL 4 GAMES</h1>
-      <p className="header__subtitle">Board Game Selector</p>
-      <ul>
-        <li>Sign Up</li>
-        <li>Login</li>
-      </ul>
+      <section className="header">
+        <Link to="/">
+          <img src={Logo} />
+        </Link>
+        <h1 className="header__h1">ROLL 4 GAMES</h1>
+        <p className="header__subtitle">Board Game Selector</p>
+        <ul>
+          <Link to="/signup">
+            <li>Sign Up</li>
+          </Link>
+          <Link to="/login">
+            <li>Login</li>
+          </Link>
+        </ul>
+      </section>
     </>
   );
 };
