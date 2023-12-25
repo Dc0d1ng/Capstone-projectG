3b171bb8cfcef0b813a63d700172977a01a739a4
Isaac Ho <Isaachwho@gmail.com>
2023-12-25 11:02:45 -0800

    add in background img, remove img tag and fix styling

--- r4g/src/Pages/ErrorPage/ErrorPage.jsx
+++ r4g/src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import "./ErrorPage.scss";
-import Ohnoes from "../../assets/12.png";
+// import Ohnoes from "../../assets/12.png";
 import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
   return (
     <section className="error">
-      <img className="error__meowsage" src={Ohnoes} />
-      <h2 className="error__header">Oh meow something went wrong!</h2>
+      <h2 className="error__header">Oh meow, something went wrong!</h2>
       <Link to="/">
         <button className="error__button">BACK TO HOME</button>
       </Link>
