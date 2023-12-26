532b0b273fd987dc3a80814bbd8d936df47f1373
Isaac Ho <Isaachwho@gmail.com>
2023-12-26 11:46:31 -0800

    adjust styling for choice layout

--- r4g/src/Pages/Choose/Choose.jsx
+++ r4g/src/Pages/Choose/Choose.jsx
@@ -4,123 +4,147 @@ import "./Choose.scss";
 
 const Choose = () => {
   const [players, setPlayers] = useState("2");
-  // const []
+  const [type, setType] = useState("Family-friendly");
+  const [difficulty, setDifficulty] = useState("Beginner");
+  const [length, setLength] = useState("15-30 mins");
 
   return (
     <>
       {/* CHOOSE PLAYERS */}
-      <h2>Choose Number of Players</h2>
-      <form>
-        <div className="radio-flexbox">
-          <label class="container">
-            2
-            <input type="radio" checked="checked" name="players" />
-            <span class="checkmark"></span>
-          </label>
-          <label class="container">
-            3
-            <input type="radio" name="players" />
-            <span class="checkmark"></span>
-          </label>
-          <label class="container">
-            4
-            <input type="radio" name="players" />
-            <span class="checkmark"></span>
-          </label>
-          <label class="container">
-            5
-            <input type="radio" name="players" />
-            <span class="checkmark"></span>
-          </label>
-          <label class="container">
-            6
-            <input type="radio" name="players" />
-            <span class="checkmark"></span>
-          </label>
-          <label class="container">
-            7
-            <input type="radio" name="players" />
-            <span class="checkmark"></span>
-          </label>
-          <label class="container">
-            8
-            <input type="radio" name="players" />
-            <span class="checkmark"></span>
-          </label>
-        </div>
-        {/* CHOOSE GAME TYPE*/}
-        <h2>Choose Type of Game</h2>
-        <div className="radio-flexbox">
-          <label class="container">
-            Strategy
-            <input type="radio" checked="checked" name="type" />
-            <span class="checkmark"></span>
-          </label>
-          <label class="container">
-            Cooperative
-            <input type="radio" name="type" />
-            <span class="checkmark"></span>
-          </label>
-          <label class="container">
-            Family Friendly
-            <input type="radio" name="type" />
-            <span class="checkmark"></span>
-          </label>
-          <label class="container">
-            Competitive
-            <input type="radio" name="type" />
-            <span class="checkmark"></span>
-          </label>
-          <label class="container">
-            Euro
-            <input type="radio" name="type" />
-            <span class="checkmark"></span>
-          </label>
-        </div>
-        {/* CHOOSE DIFFICULTY*/}
-        <h2>Choose Level of Difficulty</h2>
-        <div className="radio-flexbox">
-          <label class="container">
-            Beginner
-            <input type="radio" checked="checked" name="difficulty" />
-            <span class="checkmark"></span>
-          </label>
-          <label class="container">
-            Intermediate
-            <input type="radio" name="difficulty" />
-            <span class="checkmark"></span>
-          </label>
-          <label class="container">
-            Advanced
-            <input type="radio" name="difficulty" />
-            <span class="checkmark"></span>
-          </label>
-        </div>
-        {/* CHOOSE GAME LENGTH*/}
-        <h2>Choose Game Length</h2>
-        <div className="radio-flexbox">
-          <label class="container">
-            15-30 mins
-            <input type="radio" checked="checked" name="length" />
-            <span class="checkmark"></span>
-          </label>
-          <label class="container">
-            1/2-1 hr
-            <input type="radio" name="length" />
-            <span class="checkmark"></span>
-          </label>
-          <label class="container">
-            1-2 hrs
-            <input type="radio" name="length" />
-            <span class="checkmark"></span>
-          </label>
-          <label class="container">
-            2-4 hrs
-            <input type="radio" name="length" />
-            <span class="checkmark"></span>
-          </label>
-        </div>
-      </form>
+      <section className="choose-section">
+        <h2>Choose Number of Players</h2>
+        <form>
+          <div className="radio-flexbox">
+            <label className="container container-flex">
+              2
+              <input
+                type="radio"
+                checked={players === "2"}
+                onChange={() => setPlayers("2")}
+                name="players"
+              />
+              <span className="checkmark"></span>
+            </label>
+            <label className="container">
+              3
+              <input type="radio" name="players" />
+              <span className="checkmark"></span>
+            </label>
+            <label className="container">
+              4
+              <input type="radio" name="players" />
+              <span className="checkmark"></span>
+            </label>
+            <label className="container">
+              5
+              <input type="radio" name="players" />
+              <span className="checkmark"></span>
+            </label>
+            <label className="container">
+              6
+              <input type="radio" name="players" />
+              <span className="checkmark"></span>
+            </label>
+            <label className="container">
+              7
+              <input type="radio" name="players" />
+              <span className="checkmark"></span>
+            </label>
+            <label className="container">
+              8
+              <input type="radio" name="players" />
+              <span className="checkmark"></span>
+            </label>
+          </div>
+          {/* CHOOSE GAME TYPE*/}
+          <h2>Choose Type of Game</h2>
+          <div className="radio-flexbox">
+            <label className="container">
+              Family Friendly
+              <input
+                type="radio"
+                checked={type === "Family-friendly"}
+                onChange={() => setType("Family-friendly")}
+                name="type"
+              />
+              <span className="checkmark"></span>
+            </label>
+            <label className="container">
+              Strategy
+              <input type="radio" name="type" />
+              <span className="checkmark"></span>
+            </label>
+            <label className="container">
+              Cooperative
+              <input type="radio" name="type" />
+              <span className="checkmark"></span>
+            </label>
+            <label className="container">
+              Competitive
+              <input type="radio" name="type" />
+              <span className="checkmark"></span>
+            </label>
+            <label className="container">
+              Euro
+              <input type="radio" name="type" />
+              <span className="checkmark"></span>
+            </label>
+          </div>
+          {/* CHOOSE DIFFICULTY*/}
+          <h2>Choose Level of Difficulty</h2>
+          <div className="radio-flexbox">
+            <label className="container">
+              Beginner
+              <input
+                type="radio"
+                name="difficulty"
+                checked={difficulty === "Beginner"}
+                onChange={() => setDifficulty("Beginner")}
+              />
+              <span className="checkmark"></span>
+            </label>
+            <label className="container">
+              Intermediate
+              <input type="radio" name="difficulty" />
+              <span className="checkmark"></span>
+            </label>
+            <label className="container">
+              Advanced
+              <input type="radio" name="difficulty" />
+              <span className="checkmark"></span>
+            </label>
+          </div>
+          {/* CHOOSE GAME LENGTH*/}
+          <h2>Choose Game Length</h2>
+          <div className="radio-flexbox">
+            <label className="container">
+              15-30 mins
+              <input
+                type="radio"
+                name="length"
+                checked={length === "15-30 mins"}
+                onChange={() => setLength("15-30 mins")}
+              />
+              <span className="checkmark"></span>
+            </label>
+            <label className="container">
+              1/2-1 hr
+              <input type="radio" name="length" />
+              <span className="checkmark"></span>
+            </label>
+            <label className="container">
+              1-2 hrs
+              <input type="radio" name="length" />
+              <span className="checkmark"></span>
+            </label>
+            <label className="container">
+              2-4 hrs
+              <input type="radio" name="length" />
+              <span className="checkmark"></span>
+            </label>
+          </div>
+        </form>
+      </section>
     </>
   );
 };
