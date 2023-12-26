624158e2960ccf057132a430b2e9cea5989e02dc
Isaac Ho <Isaachwho@gmail.com>
2023-12-25 16:35:34 -0800

    create pages and general layout of choose area

--- r4g/src/App.jsx
+++ r4g/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./Pages/Landing/Landing";
 import Header from "./components/Header";
 import ErrorPage from "./Pages/ErrorPage/ErrorPage";
+import Choose from "./Pages/Choose/Choose";
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Landing />} />
-          <Route path="/choose" />
+          <Route path="/choose" element={<Choose />} />
           <Route path="/choose/selected" />
           <Route path="/user/add-fav" />
           <Route path="/user/remove-fav" />
