d981c906a71fd40829b95ba6e33192b16ce00aa9
Isaac Ho <Isaachwho@gmail.com>
2023-12-27 11:57:48 -0800

    add class names to card page

--- r4g/src/Pages/Cards/Cards.jsx
+++ r4g/src/Pages/Cards/Cards.jsx
@@ -3,63 +3,21 @@ import "./Cards.scss";
 
 const Cards = () => {
   return (
-    <>
-      <div>
-        <img src="#" />
-        <h3>Game title</h3>
-        <p>
+    <section className="cards">
+      <div className="cards__content">
+        <img className="cards__img" src="http://placekitten.com/g/100/100" />
+        <h3 className="cards__header">Game title</h3>
+        <p className="cards__text">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente
           repellat mollitia voluptas voluptates id esse eum ipsa amet, iure
           excepturi, necessitatibus nesciunt pariatur minima quo facere error
           fugiat, hic qui?
         </p>
-        <a href="#">Read More</a>
+        <a className="cards__link" href="#">
+          Read More
+        </a>
       </div>
-      <div>
-        <img src="#" />
-        <h3>Game title</h3>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente
-          repellat mollitia voluptas voluptates id esse eum ipsa amet, iure
-          excepturi, necessitatibus nesciunt pariatur minima quo facere error
-          fugiat, hic qui?
-        </p>
-        <a href="#">Read More</a>
-      </div>
-      <div>
-        <img src="#" />
-        <h3>Game title</h3>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente
-          repellat mollitia voluptas voluptates id esse eum ipsa amet, iure
-          excepturi, necessitatibus nesciunt pariatur minima quo facere error
-          fugiat, hic qui?
-        </p>
-        <a href="#">Read More</a>
-      </div>
-      <div>
-        <img src="#" />
-        <h3>Game title</h3>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente
-          repellat mollitia voluptas voluptates id esse eum ipsa amet, iure
-          excepturi, necessitatibus nesciunt pariatur minima quo facere error
-          fugiat, hic qui?
-        </p>
-        <a href="#">Read More</a>
-      </div>
-      <div>
-        <img src="#" />
-        <h3>Game title</h3>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente
-          repellat mollitia voluptas voluptates id esse eum ipsa amet, iure
-          excepturi, necessitatibus nesciunt pariatur minima quo facere error
-          fugiat, hic qui?
-        </p>
-        <a href="#">Read More</a>
-      </div>
-    </>
+    </section>
   );
 };
 
