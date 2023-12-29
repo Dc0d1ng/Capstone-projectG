d49acb1834392589d9bea91d16dfd0e22811390a
Isaac Ho <Isaachwho@gmail.com>
2023-12-29 12:15:08 -0800

    add toggle switch with styling

--- r4g/src/Pages/Landing/Landing.jsx
+++ r4g/src/Pages/Landing/Landing.jsx
@@ -1,35 +1,64 @@
+// landing.jsx
+
 import React, { useState } from "react";
 import "./landing.scss";
 import Logo from "../../assets/r4g.jpeg";
 import { Link } from "react-router-dom";
 
-const landing = () => {
+const Landing = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [isLogin, setIsLogin] = useState("");
+  const [isLogin, setIsLogin] = useState(true);
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    if (isLogin) {
+      // Implement login logic here
+      console.log("Logging in with:", username, password);
+    } else {
+      // Implement signup logic here
+      console.log("Signing up with:", username, password);
+    }
+  };
 
   return (
     <>
       <section className="landing">
         <Link to="/">
-          <img className="landing__logo" src={Logo} />
+          <img className="landing__logo" src={Logo} alt="Logo" />
         </Link>
         <h1 className="landing__h1">Board Game Selector</h1>
-        <form className="landing__form">
-          <input id="name" type="text" placeholder="Username"></input>
-          <input id="password" type="password" placeholder="Password"></input>
-          <Link to="/user">
-            <button className="landing__button landing__button-login">
-              Login
-            </button>
-          </Link>
-          {/* <Link to="/signup"> */}
-          <button className="landing__button">Sign Up</button>
-          {/* </Link> */}
+
+        <form className="landing__form" onSubmit={handleFormSubmit}>
+          <input
+            id="name"
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="Username"
+          />
+          <input
+            id="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+          />
+          <button type="submit" className="landing__button">
+            {isLogin ? "Login" : "Sign Up"}
+          </button>
         </form>
+        <button
+          className="landing__button landing__button-toggle"
+          onClick={() => setIsLogin(!isLogin)}
+        >
+          {isLogin ? "Switch to Sign Up" : "Switch to Login"}
+        </button>
         <div className="landing__divider-top"></div>
         <p className="landing__or">OR</p>
         <div className="landing__divider-bottom"></div>
+
         <Link className="start-link" to="/choose">
           Let's Begin!
         </Link>
@@ -38,4 +67,4 @@ const landing = () => {
   );
 };
 
-export default landing;
+export default Landing;
