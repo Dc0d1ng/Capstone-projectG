767341b97d014a4138a7d35fca1b106a93a03f39
Isaac Ho <Isaachwho@gmail.com>
2023-12-30 22:12:10 -0800

    fix card styles and favorites icon

--- r4g/src/Pages/Cards/Cards.jsx
+++ r4g/src/Pages/Cards/Cards.jsx
@@ -1,67 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Cards.scss";
 import { Link } from "react-router-dom";
 import { MdFavoriteBorder, MdFavorite } from "react-icons/md";
 
 const Cards = () => {
+  const [likedStates, setLikedStates] = useState([false, false, false, false]);
+
+  const handleLiked = (index) => {
+    const newLikedStates = [...likedStates];
+    newLikedStates[index] = !newLikedStates[index];
+    setLikedStates(newLikedStates);
+  };
+
   return (
     <section className="cards">
-      <div className="cards__content cards__content--selected">
-        <MdFavoriteBorder className="cards__fav-icon" />
-        <img className="cards__img" src="http://placekitten.com/g/100/100" />
-        <h3 className="cards__header">Game title</h3>
-        <p className="cards__text">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente
-          repellat mollitia voluptas voluptates id esse eum ipsa amet, iure
-          excepturi, necessitatibus nesciunt pariatur minima quo facere error
-          fugiat, hic qui?
-        </p>
-        <a className="cards__link" href="#">
-          Read More
-        </a>
-      </div>
-      <div className="cards__content">
-        <MdFavoriteBorder className="cards__fav-icon" />
-        <img className="cards__img" src="http://placekitten.com/g/100/100" />
-        <h3 className="cards__header">Game title</h3>
-        <p className="cards__text">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente
-          repellat mollitia voluptas voluptates id esse eum ipsa amet, iure
-          excepturi, necessitatibus nesciunt pariatur minima quo facere error
-          fugiat, hic qui?
-        </p>
-        <a className="cards__link" href="#">
-          Read More
-        </a>
-      </div>
-      <div className="cards__content">
-        <MdFavoriteBorder className="cards__fav-icon" />
-        <img className="cards__img" src="http://placekitten.com/g/100/100" />
-        <h3 className="cards__header">Game title</h3>
-        <p className="cards__text">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente
-          repellat mollitia voluptas voluptates id esse eum ipsa amet, iure
-          excepturi, necessitatibus nesciunt pariatur minima quo facere error
-          fugiat, hic qui?
-        </p>
-        <a className="cards__link" href="#">
-          Read More
-        </a>
-      </div>
-      <div className="cards__content">
-        <MdFavoriteBorder className="cards__fav-icon" />
-        <img className="cards__img" src="http://placekitten.com/g/100/100" />
-        <h3 className="cards__header">Game title</h3>
-        <p className="cards__text">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente
-          repellat mollitia voluptas voluptates id esse eum ipsa amet, iure
-          excepturi, necessitatibus nesciunt pariatur minima quo facere error
-          fugiat, hic qui?
-        </p>
-        <a className="cards__link" href="#">
-          Read More
-        </a>
-      </div>
+      {likedStates.map((liked, index) => (
+        <div key={index}>
+          <div
+            className={`cards__content ${
+              index === 0 ? "cards__content--selected" : ""
+            }`}
+          >
+            <div
+              className={`cards__content__fav-filled ${liked ? "liked" : ""}`}
+              onClick={() => handleLiked(index)}
+            >
+              {liked ? (
+                <MdFavorite className="cards__fav-filled" />
+              ) : (
+                <MdFavoriteBorder className="cards__fav-outline" />
+              )}
+            </div>
+
+            <img
+              className="cards__img"
+              src="http://placekitten.com/g/100/100"
+              alt={`Game ${index}`}
+            />
+            <h3 className="cards__header">Game title</h3>
+            <p className="cards__text">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente
+              repellat mollitia voluptas voluptates id esse eum ipsa amet, iure
+              excepturi, necessitatibus nesciunt pariatur minima quo facere
+              error fugiat, hic qui?
+            </p>
+            <a className="cards__link" href="#">
+              Read More
+            </a>
+          </div>
+        </div>
+      ))}
       <button className="cards__button">One More Time!</button>
       <Link to="/">
         <button className="cards__button-home">Country Roooads</button>
