bf81789a6584a1fc8e2fc35cde35091c42b93d23
Isaac Ho <Isaachwho@gmail.com>
2024-01-01 16:40:43 -0800

    data pulled from BGG api is being dynamically rendered

--- r4g/src/Pages/Cards/Cards.jsx
+++ r4g/src/Pages/Cards/Cards.jsx
@@ -1,10 +1,44 @@
-import React, { useState } from "react";
 import "./Cards.scss";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { MdFavoriteBorder, MdFavorite } from "react-icons/md";
+import axios from "axios";
 
 const Cards = () => {
-  const [likedStates, setLikedStates] = useState([false, false, false, false]);
+  const [gameData, setGameData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://www.boardgamegeek.com/xmlapi2/thing?id=123&stats=1"
+        );
+
+        const xmlData = response.data;
+
+        // Parse XML to JSON using DOMParser
+        const parser = new DOMParser();
+        const xmlDoc = parser.parseFromString(xmlData, "application/xml");
+        const gameDetails = Array.from(xmlDoc.querySelectorAll("item"));
+
+        if (gameDetails.length > 0) {
+          const thumbnail =
+            gameDetails[0].querySelector("thumbnail").textContent;
+          const gameTitle = gameDetails[0].querySelector("name").textContent;
+          const description =
+            gameDetails[0].querySelector("description").textContent;
+
+          setGameData([{ thumbnail, name: gameTitle, description }]);
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const [likedStates, setLikedStates] = useState([false]);
 
   const handleLiked = (index) => {
     const newLikedStates = [...likedStates];
@@ -14,7 +48,7 @@ const Cards = () => {
 
   return (
     <section className="cards">
-      {likedStates.map((liked, index) => (
+      {gameData.map((game, index) => (
         <div key={index}>
           <div
             className={`cards__content ${
@@ -22,10 +56,12 @@ const Cards = () => {
             }`}
           >
             <div
-              className={`cards__content__fav-filled ${liked ? "liked" : ""}`}
+              className={`cards__content__fav-filled ${
+                likedStates[index] ? "liked" : ""
+              }`}
               onClick={() => handleLiked(index)}
             >
-              {liked ? (
+              {likedStates[index] ? (
                 <MdFavorite className="cards__fav-filled" />
               ) : (
                 <MdFavoriteBorder className="cards__fav-outline" />
@@ -34,16 +70,11 @@ const Cards = () => {
 
             <img
               className="cards__img"
-              src="http://placekitten.com/g/100/100"
+              src={game.thumbnail}
               alt={`Game ${index}`}
             />
-            <h3 className="cards__header">Game title</h3>
-            <p className="cards__text">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente
-              repellat mollitia voluptas voluptates id esse eum ipsa amet, iure
-              excepturi, necessitatibus nesciunt pariatur minima quo facere
-              error fugiat, hic qui?
-            </p>
+            <h3 className="cards__header">{game.name}</h3>
+            <p className="cards__text">{game.description}</p>
             <a className="cards__link" href="#">
               Read More
             </a>
